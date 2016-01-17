8f931bf257be5ac15b99903616e42a436aced958
zmmzzmgithub <1912039529@qq.com>
2016-01-17 14:41:25 +0800

    在eclipse中使用github

---
+++ src/test/java/service/TestString.java
@@ -0,0 +1,11 @@
+package service;
+
+import org.junit.Test;
+
+public class TestString {
+	@Test
+	public void yy() {
+		String tt = "Linux 上使用Eclipse 管理github";
+		System.out.println(tt.substring(5, tt.length()));
+	}
+}
